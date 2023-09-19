19f0b75207108b2fc87a8cc6daaaceec819c8124
Nikhil Sahu <heynikhil07@gmail.com>
2023-09-19 12:45:33 +0530

    Developer Skills have been covered.

--- coding-challenge.js
+++ coding-challenge.js
@@ -1,19 +1,43 @@
 // coding challenge #1
 
 //Data -1
-const temps1 = [17, 21, 23];
+// const temps1 = [17, 21, 23];
 
-// Data-2
-const temps2 = [12, 5, -5, 0, 4];
+// // Data-2
+// const temps2 = [12, 5, -5, 0, 4];
 
-function printForecast(arr) {
-    let str = '';
-    for (let i = 0; i < arr.length; i++) {
-        str += (` ${arr[i]}°C in ${i + 1} Day ... `);
+// function printForecast(arr) {
+//     let str = '';
+//     for (let i = 0; i < arr.length; i++) {
+//         str += (` ${arr[i]}°C in ${i + 1} Day ... `);
+//     }
+//     console.log('...' + str);
+// }
+
+// printForecast(temps1);
+// printForecast(temps2);
+
+// function removeChar(str) {
+//     let newStr = '';
+//     for (let i = 0; i < str.length; i++) {
+//         if (i !== 0 || i !== str.length - 1) {
+//             newStr += str[i];
+//         }
+//     }
+//     return newStr;
+// }
+
+// console.log(removeChar('HHelloH'));
+
+function findAverage(array) {
+    // your code here
+    sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
     }
-    console.log('...' + str);
+    return sum = sum / array.length;
+    // console.log(sum);
+
 }
 
-printForecast(temps1);
-printForecast(temps2);
-
+console.log(findAverage([1, 1, 1]));
